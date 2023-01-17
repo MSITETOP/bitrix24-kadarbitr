9dadecf368f85b7325efbd4074b5935b13af3895
MrCrooK <admin@msite.top>
2023-01-17 22:03:04 +0500

    add logging

--- bitrix24kadarbitr/bitrix24kadarbitr.py
+++ bitrix24kadarbitr/bitrix24kadarbitr.py
@@ -85,7 +85,7 @@ class KadArbitrDataLoad:
 
     def getSearch(self, kad_search: str): 
         # kad_search = "А60-7141/2018, А60-27758/2019,А60-6450/2021 А60-12296/2014"
-        url = 'https://m.kad.arbitr.ru/Kad/Search';
+        url = 'https://m.kad.arbitr.ru/Kad/Search'
         data = {
             "Count" : 100, 
             "Courts" : [],
@@ -244,11 +244,11 @@ class KadArbitrDataLoad:
             except:
                 search = False
 
-            logging.debug("search: {search}".format(search)
+            logging.debug("search: {search}".format(search))
 
             if type(search) == str and len(search):
               res = self.getSearch(search)
-              logging.debug("search result: {res}".format(res)
+              logging.debug("search result: {res}".format(res))
 
               if res.get("code") == 200:
                 if self.track == True:
